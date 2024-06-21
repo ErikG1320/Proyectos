badf6cf9ba886d0215a868af09efec344e9322e1
Erik <gerik2834@gmail.com>
2024-06-21 13:11:01 -0500

    cambios sql .net

--- CRUD_MVC_NCAPAS/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ CRUD_MVC_NCAPAS/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8f57f222516e9604b493b2e120a520e9c181005")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1bbf3149e75383086cfe4b45218b530feb9191f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
