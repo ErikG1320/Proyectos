baa02d87b98ebb1943639ccc2385c1d0d41bab81
ErikG1320 <158307983+ErikG1320@users.noreply.github.com>
2024-07-17 19:06:11 -0500

    eliminacion

--- CRUD_MVC_NCAPAS/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ CRUD_MVC_NCAPAS/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af368b008d8930ed20f55fe481e47562014b8125")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ce549e42534fb0512509f6b4ace15665980712")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
