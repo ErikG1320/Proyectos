625f11f4b16767657a7b9c27a1d9d6a44cb9aae6
Erik <gerik2834@gmail.com>
2024-07-28 12:29:50 -0500

    commit de java

--- loginjavaspring/universidad/src/main/java/Controller/UsuarioController.java
+++ loginjavaspring/universidad/src/main/java/Controller/UsuarioController.java
@@ -3,7 +3,6 @@ package Controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import Entity.AuthRequest;
 
 @RestController
